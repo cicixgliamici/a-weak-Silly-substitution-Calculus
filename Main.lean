a6f31490d6fe6500f95f741d5e9b87151a99c4fc
Leonardo Chiaretti <160722347+cicixgliamici@users.noreply.github.com>
2026-04-14 10:47:27 +0200

    Create Main.lean

---
+++ Main.lean
@@ -0,0 +1,10 @@
+import SSC.Syntax
+import SSC.Contexts
+import SSC.Decompose
+import SSC.Reduction
+import SSC.Examples
+import SSC.NormalForms
+import SSC.Theorems.WeakNormalImpliesNormal
+
+def main : IO Unit :=
+  IO.println "lean-ssc loaded successfully."
